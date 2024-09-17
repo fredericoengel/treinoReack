303856190e9636cc2f84370eb59b8bc6cca1dde3
Frederico <fredericoengel@proton.me>
2024-09-17 11:57:55 -0300

    ci: Add publish storybook workflow

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import './App.css';
 export default function App() {
   const title = 'BEM VINDO AO REACT FDP DOS INFERNO!';
   const likes = 50;
+  const nome = 'Frederico Engel';
+  const idade = 40;
   return (
     <div className='App'>
       <div className='content'>
         <h1>{ title }</h1>
         <p> Liked {likes} times</p>
+        <p> Meu nome é { nome } e tenho {idade} anos.</p>
       </div>
     </div>
   )
